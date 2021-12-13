f68f9f451756e1fbc29541af12fee80621fca909
Jun Nishimura <n.junjun0303@gmail.com>
2021-12-13 15:18:41 +0900

    update routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '@/views/Home.vue'
-import Idea1 from '@/views/Idea1.vue'
-import Idea2 from '@/views/Idea2.vue'
-
 Vue.use(VueRouter)
 
 const routes = [
@@ -11,16 +8,6 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
-  },
-  {
-    path: '/idea1',
-    name: 'Idea1',
-    component: Idea1
-  },
-  {
-    path: '/idea2',
-    name: 'Idea2',
-    component: Idea2
   }
 ]
 
